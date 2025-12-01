ba202f1ab17a6f29691def2937aa3b056f532c9b
mokshagnachintha <43058381+mokshagnachintha@users.noreply.github.com>
2025-12-01 18:53:07 +0530

    test modified

--- test.py
+++ test.py
@@ -1,12 +1,1 @@
-import pytest
-from app import app
-
-@pytest.fixture
-def client():
-    app.testing = True
-    return app.test_client()
-
-def test_home_route(client):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.data == b"Hello, Flask!"
+print('hello world')
